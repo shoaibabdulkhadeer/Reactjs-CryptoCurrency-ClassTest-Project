95603ecfbc52da25c0cee07407186a9aa04235ad
Shoaib Abdul Khadeer <shoaibabdulkhadeer@gmail.com>
2023-04-11 13:50:21 +0530

    minor refractory

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import CoinsItem from './components/CoinsItem';
+
+
+
 
 function App() {
+
+const [coins,setCoins] = useState([])
+
+useEffect(() => {
+ axios.get('https://api.coinranking.com/v2/coins').then((response) =>{
+  setCoins(response.data.data.coins)
+  console.log(response.data.data.coins)
+ })
+},[])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+       <CoinsItem  coins = {coins}/>
+
+    </div> 
   );
 }
 
---
+++ src/components/CoinsItem.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import '../styles/Coins.css'
+
+const CoinsItem = ({coins}) => {
+
+  return (
+    <div className='CoinsItem'>
+
+   <div className='main'>
+       <h2 className='bluehead'>SECURLY BUY,SELL,STORE,SEND YOUR CODE</h2>
+        <h1> Buy crypto at true cost </h1>
+        <p className='para'>Buy add sell 250+ cryptocurrencies with 20+ list cryptocurrencies
+            using bank transfer to your credit/debit card
+        </p>
+
+</div>   
+        <table className=''>
+        <tbody >
+           {coins.map((coin) => {
+
+         const myString = coin.price;
+        const newNumber = Number(myString).toFixed(2);
+
+            return (
+                <>
+              <tr className='row'>
+                
+              <td>{coin.name}</td>
+              <td className='symbol'>{coin.symbol}</td>
+              <td className='number'>${newNumber}</td>
+              <td style={{color:"lightgreen"}}>{coin.change}%</td>
+              <td>${coin.price} </td>
+              <td><button>Trade</button></td>
+              
+              </tr>
+            
+         
+              </>
+            )
+           }) }
+        </tbody>
+      </table>
+        
+     </div>
+  )
+}
+
+export default CoinsItem
